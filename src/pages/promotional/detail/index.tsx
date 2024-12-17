ad9a0d6a2f00dcba15aa02a4104bd034cd210a4d
pvq2k2 <vanquyet2392002@gmail.com>
2024-12-17 23:20:27 +0700

    promotional detail ui

--- src/pages/promotional/detail/index.tsx
+++ src/pages/promotional/detail/index.tsx
@@ -1,13 +1,14 @@
-import { ArrowLeftOutlined, DeleteFilled, EditFilled } from "@ant-design/icons";
-import { Button, Space, TableProps } from "antd";
+import { DeleteOutlined, EditOutlined, LeftOutlined, PlusOutlined } from "@ant-design/icons";
+import { Button, Popconfirm, Space, TableProps, Tooltip } from "antd";
+import { CommonSearch, CommonTable } from "components/common";
 import PromoProductModal from "components/Modal/PromotionalProduct";
 import NumberFormat from "components/NumberFormat";
-import TableSearch from "components/TableSearch";
+import { APP_NAME } from "constant";
 import { initPromoProduct, initPromotional } from "constant/initial";
 import { IPromoProduct, IPromotional } from "constant/interface";
 import { ModalType, ModalTypeType } from "constant/type";
 import { FC, useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { ROUTE_URL } from "routes";
 import { promotionalService } from "services/promotional";
 
@@ -23,12 +24,26 @@ const PromotionalDetail: FC = () => {
     list: [],
     promotional: initPromotional,
   });
+
+  const [originalPromoProducts, setOriginalPromoProducts] = useState<{
+    list: Array<IPromoProduct>;
+    promotional: IPromotional;
+  }>({
+    list: [],
+    promotional: initPromotional,
+  });
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
   const { id } = useParams();
+  const navigate = useNavigate();
   const fetchData = async () => {
+    setIsLoading(true);
     const res = await promotionalService.getListProductPromotional({
       id: id ? +id : "",
     });
     setPromoProducts(res);
+    setOriginalPromoProducts(res);
+    setIsLoading(false);
   };
   const handleOpenModal = (status: ModalTypeType, item: IPromoProduct) => {
     setOpen({
@@ -36,6 +51,10 @@ const PromotionalDetail: FC = () => {
       item,
     });
   };
+
+  const handleSetPromoProducts= (data: Array<IPromoProduct>) => {
+    setPromoProducts(pre => ({...pre, list: data}))
+  }
   const handleDeletePromotional = async (id: string | number) => {
     await promotionalService.deleteProductPromotional({ id });
     await fetchData();
@@ -81,64 +100,100 @@ const PromotionalDetail: FC = () => {
       key: "saleTotal",
     },
     {
-      title: "Action",
+      title: "Hành động",
       dataIndex: "id",
       key: "id",
       render: (value: string | number, rec: IPromoProduct) => {
         return (
           <Space>
-            <Button
-              type="link"
-              size="small"
-              onClick={() => {
-                handleOpenModal("edit", rec);
-              }}
-            >
-              <EditFilled />
-            </Button>
-            <Button
-              type="link"
-              size="small"
-              danger
-              onClick={() => {
-                handleDeletePromotional(value);
-              }}
-            >
-              <DeleteFilled />
-            </Button>
+            <Tooltip placement="top" title="Sửa">
+              <Button
+                type="link"
+                size="small"
+                icon={<EditOutlined />}
+                onClick={() => {
+                  handleOpenModal("edit", rec);
+                }}
+              />
+            </Tooltip>
+
+            <Tooltip placement="top" title="Xóa">
+              <Popconfirm
+                title={`Xóa ${rec.name}`}
+                description="Bạn có chắc chắn muốn xóa !"
+                onConfirm={() => handleDeletePromotional(value)}
+                okText="Có"
+                cancelText="Không"
+              >
+                <Button
+                  icon={<DeleteOutlined />}
+                  type="link"
+                  size="small"
+                  danger
+                />
+              </Popconfirm>
+            </Tooltip>
           </Space>
         );
       },
     },
   ];
   useEffect(() => {
+    document.title = `Chương trình khuyến mãi - ${APP_NAME}`;
     if (id) fetchData();
   }, [id]);
   return (
     <div className="promotional-detail">
-      <Link to={ROUTE_URL.PROMOTIONAL}>
-        <h1 style={{ cursor: "pointer" }}>
-          <ArrowLeftOutlined /> Chương trình khuyến mãi
+      <div className="table-headding">
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <Tooltip placement="top" title="Quay về">
+          <Button
+            type="link"
+            size="large"
+            icon={<LeftOutlined />}
+            onClick={() => {
+              navigate(ROUTE_URL.PROMOTIONAL);
+            }}
+          />
+        </Tooltip>
+
+        <h1>
+        Chương trình khuyến mãi
         </h1>
-      </Link>
-      <div style={{ margin: "10px 0" }}>
-        <h2 style={{ marginBottom: "10px" }}>
+      </div>
+      <Button
+          type="default"
+          color="default"
+          className="btn-add"
+          size="large"
+          onClick={() => handleOpenModal("create", initPromoProduct)}
+          icon={<PlusOutlined />}
+        >
+          Tạo chương trình
+        </Button>
+      </div>
+      <CommonSearch
+        originalData={originalPromoProducts.list}
+        data={promoProducts.list}
+        setData={handleSetPromoProducts}
+        onRefresh={fetchData}
+      />
+      <div style={{ paddingBottom: 20, display: 'flex', alignItems: 'center', gap: 10 }}>
+        <h2>
           {promoProducts.promotional.name}
         </h2>
         <p>
-          Thời gian: {promoProducts.promotional.startDate} -{" "}
+          Thời gian: {promoProducts.promotional.startDate} - {" "}
           {promoProducts.promotional.endDate}
         </p>
       </div>
-      <Button
-        type="primary"
-        onClick={() => {
-          handleOpenModal("create", initPromoProduct);
-        }}
-      >
-        Thêm sản phẩm
-      </Button>
-      <TableSearch columns={columns} data={promoProducts.list} />
+      <CommonTable
+        originalData={originalPromoProducts.list}
+        data={promoProducts.list}
+        setData={handleSetPromoProducts}
+        isLoading={isLoading}
+        columns={columns}
+      />
       <PromoProductModal
         open={open}
         setOpen={setOpen}
