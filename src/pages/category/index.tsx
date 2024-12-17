8f8abf044f293657c2971120916ed2e8da7b4f44
pvq2k2 <vanquyet2392002@gmail.com>
2024-12-17 20:27:47 +0700

    category ui

--- src/pages/category/index.tsx
+++ src/pages/category/index.tsx
@@ -1,8 +1,9 @@
-import { RedoOutlined } from "@ant-design/icons";
-import { Button, Space, TableProps } from "antd";
+import { DeleteOutlined, EditOutlined, PlusOutlined } from "@ant-design/icons";
+import { Button, Popconfirm, Space, TableProps, Tooltip } from "antd";
+import { CommonSearch, CommonTable } from "components/common";
 import CategoryModal from "components/Modal/Category";
-import TableSearch from "components/TableSearch";
-import { initProduct } from "constant/initial";
+import { APP_NAME } from "constant";
+import { initCategory } from "constant/initial";
 import { ICategory } from "constant/interface";
 import { ModalType, ModalTypeType } from "constant/type";
 import { FC, useEffect, useState } from "react";
@@ -11,9 +12,11 @@ import { categoryService } from "services/category";
 const Category: FC = () => {
   const [open, setOpen] = useState<ModalType<ICategory>>({
     type: "",
-    item: initProduct,
+    item: initCategory,
   });
   const [categories, setCategories] = useState<Array<ICategory>>([]);
+  const [originalCategories, setOriginalCategories] = useState<Array<ICategory>>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const handleOpenModal = (status: ModalTypeType, item: ICategory) => {
     setOpen({
       type: status,
@@ -48,51 +51,78 @@ const Category: FC = () => {
       render: (value: string | number, rec: ICategory) => {
         return (
           <Space>
-            <Button
-              type="primary"
-              onClick={() => {
-                handleOpenModal("edit", rec);
-              }}
-            >
-              Edit
-            </Button>
-            <Button
-              type="primary"
-              danger
-              onClick={() => {
-                handleDeleteCategory(value);
-              }}
-            >
-              Delete
-            </Button>
+            <Tooltip placement="top" title="Sửa">
+              <Button
+                type="link"
+                size="small"
+                icon={<EditOutlined />}
+                onClick={() => {
+                  handleOpenModal("edit", rec);
+                }}
+              />
+            </Tooltip>
+
+            <Tooltip placement="top" title="Xóa">
+              <Popconfirm
+                title={`Xóa ${rec.name}`}
+                description="Bạn có chắc chắn muốn xóa !"
+                onConfirm={() => handleDeleteCategory(value)}
+                okText="Có"
+                cancelText="Không"
+              >
+                <Button
+                  icon={<DeleteOutlined />}
+                  type="link"
+                  size="small"
+                  danger
+                />
+              </Popconfirm>
+            </Tooltip>
           </Space>
         );
       },
     },
   ];
   const getListCategory = async () => {
+    setIsLoading(true);
     const res = await categoryService.getListCategory();
     setCategories(res);
+    setOriginalCategories(res);
+    setIsLoading(false);
   };
   useEffect(() => {
+    document.title = `Quản lý loại hàng - ${APP_NAME}`;
     getListCategory();
   }, []);
   return (
     <div className="product-page">
-      <h1 style={{ marginBottom: 20 }}>Quản lý loại hàng</h1>
-      <Button
-        type="primary"
-        size="large"
-        onClick={() => {
-          handleOpenModal("create", initProduct);
-        }}
-      >
-        Tạo loại hàng
-      </Button>
-      <Button type="link" onClick={getListCategory}>
-        <RedoOutlined />
-      </Button>
-      <TableSearch columns={columns} data={categories} />
+      <div className="table-headding">
+        <h1>Quản lý loại hàng</h1>
+        <Button
+          type="default"
+          color="default"
+          className="btn-add"
+          size="large"
+          onClick={() => handleOpenModal("create", initCategory)}
+          icon={<PlusOutlined />}
+        >
+          Tạo loại hàng
+        </Button>
+      </div>
+
+      <CommonSearch
+        originalData={originalCategories}
+        data={categories}
+        setData={setCategories}
+        onRefresh={getListCategory}
+      />
+      <CommonTable
+        originalData={originalCategories}
+        data={categories}
+        setData={setCategories}
+        isLoading={isLoading}
+        columns={columns}
+      />
       <CategoryModal open={open} setOpen={setOpen} />
     </div>
   );
